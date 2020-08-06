19499bca48c5be5b65bb1105909841060dd6dcee
yoyoblackrow <youssefosama200@ymail.com>
2020-08-06 20:41:34 +0200

    this is the third commit

--- app/src/main/java/com/example/gittoandroidstudio/MainActivity.java
+++ app/src/main/java/com/example/gittoandroidstudio/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("first TAG","massage");
+        Log.i("first TAG","massage");
+        Log.i("first TAG","massage");
     }
 }
